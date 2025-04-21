cb1cec06028e50d91e768eedbe972ffb9ed14ac9
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2025-04-21 11:05:46 +0500

    PORT UPDATED

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
   connectDb();
 });
