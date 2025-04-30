6a36073072e10b3dac1090762ddc16935e9cc4bb
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2025-04-30 16:50:02 +0500

    update job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -89,14 +89,8 @@ const updateJob = asyncHandler(async (req, res) => {
 // @route   DELETE /api/jobs/:id
 // @access  Private/Admin
 const deleteJob = asyncHandler(async (req, res) => {
-  const job = await Job.findById(req.params.id);
+  const job = await Job.findByIdAndDelete(req.params.id);
 
-  if (!job) {
-    res.status(404);
-    throw new Error("Job not found");
-  }
-
-  await job.remove();
   res.json({ message: "Job removed" });
 });
 
