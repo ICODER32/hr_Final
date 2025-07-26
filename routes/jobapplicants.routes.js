f41788a8552a076f6f130300bf97c4ebdf4009ec
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2025-07-26 16:27:05 +0500

    updated

--- routes/jobapplicants.routes.js
+++ routes/jobapplicants.routes.js
@@ -9,6 +9,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/resumes/");
   },
+
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(
